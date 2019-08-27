6586cdca2b7d9797b0bb4b16878681999869d6af
Lucas Dachman <lucas.dachman@gmail.com>
2019-08-27 16:10:28 -0600

    first knob

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Nexus = window.Nexus;
+
+const App = () => {
+
+  const [val, setVal] = useState(null);
+
+  useEffect(() => {
+    const dial = new Nexus.Dial('#dial');
+    dial.on('change', v => setVal(v));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header>Hello, world</header>
+      <div id='dial'></div>
+      <h1>{val}</h1>
     </div>
   );
 }
