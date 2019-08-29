de46768fa3934a2afb62b4ce9ccc5ef84a7aeec6
Lucas Dachman <lucas.dachman@gmail.com>
2019-08-29 17:56:38 -0600

    basic adsr synth

--- src/audio/CSynth.js
+++ src/audio/CSynth.js
@@ -1,17 +1,67 @@
 import Tone from 'tone';
 
 export default class CSynth {
-  constructor() {
-    this.synth = new Tone.PolySynth(8, Tone.Synth, {
+  constructor(bpm) {
+    // this.loop.humanize = true;
+    Tone.Transport.bpm.value = bpm;
+    Tone.Transport.start();
+
+    this.synth = new Tone.PolySynth(6, Tone.MonoSynth).toMaster();
+    this.synth.set({
       oscillator: {
         type: 'sawtooth'
+      },
+      filter: {
+        Q: 0
+      },
+      filterEnvelope: {
+        baseFrequency: 100,
+        octaves: 4.5,
+        release: 10
       }
-    })
-      .toMaster();
+    });
+    this.chords = new Array(16)
+      .fill(null).map(v => []);
+    this.step = 0;
+
+    this.loop = new Tone.Loop(this.loopCallback, '16n')
+  }
+
+  loopCallback = (time) => {
+    this._onTick(time, this.step);
+    const nextChord = this.nextNotes();
+    // console.log('next chord:', nextChord)
+    nextChord.length > 0 &&
+      this.synth.triggerAttackRelease(nextChord, '16n', time);
+  }
+
+  _onTick(time, step) {
+    if (!this.onTick) return;
+    Tone.Draw.schedule(() => {
+      this.onTick(step);
+    }, time + 0.1)
+  }
+
+  nextNotes() {
+    let nextChord = this.chords[this.step] || [];
+    this.step++;
+    if (this.step >= this.chords.length) this.step = 0;
+    // find probabilities for each note
+    return nextChord;
+  }
+
+  addNote({ index, freq }) {
+    const chord = this.chords[index];
+    if (!chord.includes(freq)) {
+      chord.push(freq);
+    }
+  }
+
+  removeNote({ index, freq }) {
+    this.chords[index] = this.chords[index].filter(value => value !== freq);
   }
 
   triggerAttack(frequency) {
-    console.log('attack', frequency)
     this.synth.triggerAttack(frequency);
   }
 
@@ -19,11 +69,51 @@ export default class CSynth {
     this.synth.triggerRelease(frequency);
   }
 
+  setNotes(chords) {
+    this.chords = chords;
+  }
+
+  setAmpAttack(ratio) {
+    this.synth.set({
+      envelope: {
+        attack: ratio + 0.01
+      }
+    });
+  }
+
+  setAmpDecay(ratio) {
+    this.synth.set({
+      envelope: {
+        decay: ratio + 0.01
+      }
+    });
+  }
+
+  setAmpSustain(ratio) {
+    this.synth.set({
+      envelope: {
+        sustain: ratio
+      }
+    });
+  }
+
+  setAmpRelease(ratio) {
+    this.synth.set({
+      envelope: {
+        release: (ratio * 10) + 0.01
+      }
+    });
+  }
+
   start() {
     Tone.context.resume();
+    // Tone.Transport.start();
+    this.loop.start();
   }
 
   stop() {
-    this.synth.releaseAll();
+    // Tone.Transport.stop();
+    this.loop.stop();
+    this.step = 0;
   }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import startAudioContext from 'startaudiocontext';
+import Tone from 'tone';
 import CSynth from '../audio/CSynth';
 import './App.css';
 
@@ -7,55 +9,88 @@ const { mtof } = Nexus;
 
 const lowNote = 36;
 const highNote = 72;
+const tickTime = 170;
 
 const App = () => {
 
   const [val, setVal] = useState(null);
+  const [audioContextStarted, setAudioContextStarted] = useState(false);
 
-  // MUI elements
+  // runs once after first render
   useEffect(() => {
-    const synth = new CSynth();
-
-    const dial = new Nexus.Dial('#dial');
-    dial.on('change', v => setVal(v));
-
-    const piano = new Nexus.Piano('#piano', { lowNote, highNote });
-    piano.on('change', ({ note, state }) => {
-      if (state === true)
-        synth.triggerAttack(mtof(note));
-      else
-        synth.triggerRelease(mtof(note));
+    startAudioContext(Tone.context).then(() => {
+      setAudioContextStarted(true);
     });
+  }, []);
+
+  // runs when audio context has started
+  useEffect(() => {
+    if (!audioContextStarted) return;
+    const synth = new CSynth(tickTime);
+
+    // const piano = new Nexus.Piano('#piano', { lowNote, highNote });
+    // piano.on('change', ({ note, state }) => {
+    //   if (state === true)
+    //     synth.triggerAttack(mtof(note));
+    //   else
+    //     synth.triggerRelease(mtof(note));
+    // });
 
     const sequencer = new Nexus.Sequencer('#sequencer', {
       rows: 12,
       columns: 16
     });
-    sequencer.on('step', (noteVals) => {
-      const noteOns = noteVals.map((v, i) => v ? mtof(lowNote + i) : null).filter(v => v);
-      const noteOffs = noteVals.map((v, i) => v ? null : mtof(lowNote + i)).filter(v => v);
-      noteOns.length > 0 && synth.triggerAttack(noteOns);
-      noteOffs.length > 0 && synth.triggerRelease(noteOffs);
+    sequencer.on('change', ({ row, column: index, state }) => {
+      const freq = mtof(lowNote - row + 24);
+      if (state) {
+        synth.addNote({ index, freq });
+      } else {
+        synth.removeNote({ index, freq });
+      }
     });
+    synth.onTick = (pos) => {
+      sequencer.stepper.value = pos;
+      sequencer.next();
+    }
 
     const playButton = new Nexus.Button('#play-button', { mode: 'toggle' });
-    playButton.on('change', state => {
-      if (state) {
-        sequencer.start(170);
-        synth.start();
-      }
-      else {
-        sequencer.stepper.value = 0;
-        sequencer.stop();
-        synth.stop();
-      }
-    });
-  }, []);
+    playButton.on('change', state => state ? synth.start() : synth.stop());
 
-  return (
+    // KNOBS
+    const ampAttack = new Nexus.Dial('#amp-attack');
+    ampAttack.on('change', v => synth.setAmpAttack(v));
+
+    const ampDecay = new Nexus.Dial('#amp-decay');
+    ampDecay.on('change', v => synth.setAmpDecay(v));
+
+    const ampSustain = new Nexus.Dial('#amp-sustain');
+    ampSustain.on('change', v => synth.setAmpSustain(v));
+
+    const ampRelease = new Nexus.Dial('#amp-release');
+    ampRelease.on('change', v => synth.setAmpRelease(v));
+
+  }, [audioContextStarted]);
+
+  // actual render code
+
+  return !audioContextStarted ? 'Click' : (
     <div className='App'>
-      <div id='dial'></div>
-      <div id='piano'></div>
+      <section id='amp-env'>
+        <div id='amp-attack'></div>
+        <div id='amp-decay'></div>
+        <div id='amp-sustain'></div>
+        <div id='amp-release'></div>
+      </section>
+      {/* <section id='filter-env'>
+        <div id='filter-attack'></div>
+        <div id='filter-decay'></div>
+        <div id='filter-sustain'></div>
+        <div id='filter-release'></div>
+        <div id='filter-min'></div>
+        <div id='filter-max'></div>
+        <div id='filter-q'></div>
+      </section> */}
+      {/* <div id='piano'></div> */}
       <div id='play-button'></div>
       <div id='sequencer'></div>
     </div>
