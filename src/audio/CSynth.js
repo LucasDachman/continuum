0c3e872239b81f1df7131bbc1a1033b0e07b79f2
Lucas Dachman <lucas.dachman@gmail.com>
2019-08-28 18:15:26 -0600

    trigger notes from sequencer

--- src/audio/CSynth.js
+++ src/audio/CSynth.js
@@ -2,15 +2,28 @@ import Tone from 'tone';
 
 export default class CSynth {
   constructor() {
-    this.synth = new Tone.PolySynth(4, Tone.Synth)
+    this.synth = new Tone.PolySynth(8, Tone.Synth, {
+      oscillator: {
+        type: 'sawtooth'
+      }
+    })
       .toMaster();
   }
 
   triggerAttack(frequency) {
+    console.log('attack', frequency)
     this.synth.triggerAttack(frequency);
   }
 
   triggerRelease(frequency) {
     this.synth.triggerRelease(frequency);
+  }
+
+  start() {
+    Tone.context.resume();
+  }
+
+  stop() {
+    this.synth.releaseAll();
   }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import './App.css';
 const Nexus = window.Nexus;
 const { mtof } = Nexus;
 
+const lowNote = 36;
+const highNote = 72;
+
 const App = () => {
 
   const [val, setVal] = useState(null);
@@ -16,23 +19,45 @@ const App = () => {
     const dial = new Nexus.Dial('#dial');
     dial.on('change', v => setVal(v));
 
-    const piano = new Nexus.Piano('#piano', {
-      lowNote: 36,
-      highNote: 72,
-    });
+    const piano = new Nexus.Piano('#piano', { lowNote, highNote });
     piano.on('change', ({ note, state }) => {
-      if(state === true)
+      if (state === true)
         synth.triggerAttack(mtof(note));
       else
         synth.triggerRelease(mtof(note));
+    });
+
+    const sequencer = new Nexus.Sequencer('#sequencer', {
+      rows: 12,
+      columns: 16
+    });
+    sequencer.on('step', (noteVals) => {
+      const noteOns = noteVals.map((v, i) => v ? mtof(lowNote + i) : null).filter(v => v);
+      const noteOffs = noteVals.map((v, i) => v ? null : mtof(lowNote + i)).filter(v => v);
+      noteOns.length > 0 && synth.triggerAttack(noteOns);
+      noteOffs.length > 0 && synth.triggerRelease(noteOffs);
+    });
+
+    const playButton = new Nexus.Button('#play-button', { mode: 'toggle' });
+    playButton.on('change', state => {
+      if (state) {
+        sequencer.start(170);
+        synth.start();
+      }
+      else {
+        sequencer.stepper.value = 0;
+        sequencer.stop();
+        synth.stop();
+      }
     });
   }, []);
 
   return (
     <div className='App'>
-      <header>Hello, world</header>
       <div id='dial'></div>
       <div id='piano'></div>
+      <div id='play-button'></div>
+      <div id='sequencer'></div>
     </div>
   );
 }
