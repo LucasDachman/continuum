fada9261990da6441af467b2a9e61bf1c0098219
Lucas Dachman <lucas.dachman@gmail.com>
2019-09-17 16:17:12 -0600

    prepare server for build but it doesnt work fuck you babel 7

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,32 @@
-module.exports = {
-  presets: ['@babel/preset-env'],
-  babelrcRoots: [
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/env',
+      {
+        'targets': {
+          'node': true,
+          'esmodules': true
+        },
+        'modules': 'commonjs'
+      }
+    ]
+  ];
+  const plugins = ["@babel/plugin-transform-modules-commonjs"];
+
+  const babelrcRoots = [
     ".",
     "shared/*",
     "server/*"
-  ]
+  ];
+
+  const ignore = ['**/node_modules'];
+
+  return {
+    presets,
+    plugins,
+    babelrcRoots,
+    ignore
+  };
 }
